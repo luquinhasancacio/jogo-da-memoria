a6d07093498cd2150f180d151a07e6a86ecb141f
Lucas Ancacio <152306830+luquinhasancacio@users.noreply.github.com>
2023-11-28 12:44:22 -0300

    Create akali.py
    Adição de arquivo python da Akali.

---
+++ akali.py
@@ -0,0 +1,13 @@
+from random import randint
+
+emocoes = ['feliz', 'triste', 'dengosa']
+
+emocao_akali = emocoes[randint(0,2)]
+
+print('───▐▀▄─────────▄▀▌')
+print('───▐▓░▀▄▀▀▀▀▀▄▀░▓▌ ')
+print(f'───▐░▓░▄▀░░░▀▄░▓░▌    Akali está {emocao_akali}')
+print('────█░░▌█▐░▌█▐░░█────▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
+print('─▄▄▄▐▀░░░▀█▀░░░▀▌▄▄▄')
+print('█▐▐▐▌▀▄░▀▄▀▄▀░▄▀▐▌▌▌█')
+print('▀▀▀▀▀▀▀▀▀▄▄▄▀▀▀▀▀▀▀▀▀')
